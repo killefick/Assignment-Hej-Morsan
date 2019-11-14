10553f1e2f39bff9a187945096dc2bb098dab99b
nilabr <grunau@gmx.de>
2019-11-14 12:58:50 +0100

    empty

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hej_Morsan_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
