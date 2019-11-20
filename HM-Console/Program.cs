ee5953d44504d091a5b6d4a3c65bf2df53bd2610
gwestberg <ondurdis1985@gmail.com>
2019-11-20 11:07:28 +0100

    Added stuff

--- HM-Console/Program.cs
+++ HM-Console/Program.cs
@@ -33,18 +33,18 @@ namespace ClassLibHejMorsan
 
             //List of persons that is supposed to connect to the database
             List<Person> PplToCall = new List<Person>();
-            // PplToCall.Add(Mamma);
-            // PplToCall.Add(Pappa);
-            // PplToCall.Add(Syster);
-            // PplToCall.Add(Mormor);
+            PplToCall.Add(Mamma);
+            PplToCall.Add(Pappa);
+            PplToCall.Add(Syster);
+            PplToCall.Add(Mormor);
             // creates connection object to connect to database
-            var db = new DB("Server=40.85.84.155;Database=Student13;User=Student13;Password=YH-student@2019;");
+            // var db = new DB("Server=40.85.84.155;Database=Student13;User=Student13;Password=YH-student@2019;");
 
-            // adds persons from databas to list
-            foreach (var item in db.GetPersons())
-            {
-                PplToCall.Add(item);
-            }
+            // // adds persons from databas to list
+            // foreach (var item in db.GetPersons())
+            // {
+            //     PplToCall.Add(item);
+            // }
 
             int day = 0;
             while (true)
@@ -74,6 +74,7 @@ namespace ClassLibHejMorsan
                     person.TimeToCallMom();
                     if (person.TimeToCallMom() == true)
                     {
+                        
                         int choice;
                         System.Console.WriteLine($"Time to call {person.name}");
                         System.Console.WriteLine("Press 1 to call.");
@@ -83,7 +84,7 @@ namespace ClassLibHejMorsan
                         }
                         catch
                         {
-                            System.Console.WriteLine($"You chose to not call {person.name}");
+                            System.Console.WriteLine($"You chose to not call {person.name}\n");
                             choice = 0;
                         }
                         person.ChooseToCallMom(choice);
