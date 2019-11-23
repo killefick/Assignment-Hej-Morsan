7ed18691dc1db1f9a5226f4a1996539cf8f9fabe
nilabr <grunau@gmx.de>
2019-11-23 13:11:08 +0100

    added update page

---
+++ HM-Webapp/Pages/UpdatePerson.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ClassLibHejMorsan;
+
+namespace Hej_Morsan_Projekt.Pages
+{
+    public class UpdatePersonModel : PageModel
+    {
+         // instantiate class
+        Person person = new Person();
+
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Phone { get; private set; }
+        public string Birthday { get; private set; }
+        public string Counter { get; private set; }
+
+        public void OnGet()
+        {
+            Id = Request.Query["id"];
+            Name = Request.Query["name"];
+            Phone = Request.Query["phone"];
+            Birthday = Request.Query["birthday"];
+            Counter = Request.Query["counter"];
+        }
+    }
+}
